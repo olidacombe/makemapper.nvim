b0edd74506681c7a6f3d99b44659c68faaf2c8f9
Oli Dacombe <odacombe@blockchain.com>
2023-02-08 00:25:14 +0000

    Sketch out some structure before all the treesitter fun

--- lua/makemapper/init.lua
+++ lua/makemapper/init.lua
@@ -1,3 +1,30 @@
 local M = {}
 
+local default_opts = {
+    prefix = "<leader>M"
+}
+
+local opts
+
+local set_mappings = function(m)
+    local mappings = m or {}
+    for target, suffix in pairs(mappings) do
+        vim.keymap.set("n", opts.prefix .. suffix, "! make " .. target, { desc = target })
+    end
+end
+
+M.setup = function(o)
+    opts = vim.tbl_deep_extend("force", {}, default_opts, o or {})
+
+    -- TODO __if whichkey exists__
+    -- will have to split opts.prefix appropriately first
+    -- require("which-key").register({
+    --     m = {
+    --         name = "make"
+    --     }
+    -- }, { prefix = "<leader>" })
+
+    set_mappings(require "makemapper.makefile".parse_mappings())
+end
+
 return M
---
+++ lua/makemapper/makefile.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+-- finds annotations in `Makefile` and returns a table of
+-- suffix -> target
+-- assignments
+M.parse_mappings = function()
+    return {}
+end
+
+return M
