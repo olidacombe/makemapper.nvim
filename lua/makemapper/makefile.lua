1c8385fb84492bce8f68807889368fa1d4331f81
Oli Dacombe <odacombe@blockchain.com>
2023-02-09 23:34:32 +0000

    Start on some functionality

--- lua/makemapper/makefile.lua
+++ lua/makemapper/makefile.lua
@@ -1,10 +1,117 @@
 local M = {}
 
+-- see if the file exists
+local file_exists = function(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+local lines_from = function(file)
+    if not file_exists(file) then return nil end
+    local lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+-- load Makefile into a buffer and return the id,
+-- or nil if not found
+M.makefile_buffer = function()
+    local cwd = vim.fn.getcwd()
+    local makefile_path = cwd .. "/Makefile"
+    local lines = lines_from(makefile_path)
+    if lines == nil then return end
+    -- create unlisted scratch buffer
+    local buf = vim.api.nvim_create_buf(false, true)
+    -- load lines of Makefile into it
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    -- return handle
+    return buf
+end
+
 -- finds annotations in `Makefile` and returns a table of
 -- suffix -> target
 -- assignments
 M.parse_mappings = function()
-    return {}
+    local makefile = M.makefile_buffer()
+    if not makefile then return {} end
+    return M.parse_buffer(makefile)
+end
+
+local node_text = function(node, ctx)
+    local r1, c1, r2, c2 = node:range()
+    local text = vim.api.nvim_buf_get_text(ctx.bufnr or 0, r1, c1, r2, c2, {})
+    -- get first line
+    return table.remove(text)
+end
+
+M.parse_rule_node = function(node, ctx)
+    ctx = ctx or {}
+    ctx.mappings = ctx.mappings or {}
+    ctx.bufnr = ctx.bufnr or 0
+
+    local target
+    for child in node:iter_children() do
+        local type = child:type()
+        if type == "comment" then
+            M.parse_comment_node(child, ctx)
+        elseif type == "targets" then
+            target = node_text(child, ctx)
+        end
+    end
+    if target then
+        ctx.mappings[target] = ctx.current_mapping_annotation
+        ctx.current_mapping_annotation = nil
+    end
+end
+
+M.parse_comment = function(str)
+    local _, _, annotation = string.find(str, "nvim_map%((%S+)%)")
+    return annotation
+end
+
+M.parse_comment_node = function(node, ctx)
+    ctx = ctx or {}
+    local annotation = M.parse_comment(node_text(node, ctx) or "")
+    -- update current_mapping_annotation if the current comment has valid content
+    ctx.current_mapping_annotation = annotation or ctx.current_mapping_annotation
+end
+
+M.parse_node = function(node, ctx)
+    ctx = ctx or {}
+    local type = node:type()
+
+    if type == "comment" then
+        M.parse_comment_node(node, ctx)
+    elseif type == "rule" then
+        M.parse_rule_node(node, ctx)
+    else
+        -- whatever annotation we may have last seen is not for this node
+        -- or the next
+        ctx.current_mapping_annotation = nil
+    end
+end
+
+-- get a table of mappings from a buffer
+-- with entries of the form `target = suffix`
+M.parse_buffer = function(bufnr)
+    local tsparser = vim.treesitter.get_parser(bufnr or 0, "make")
+    local trees = tsparser:parse()
+    local mappings = {}
+    if #trees < 1 then return mappings end
+    local tree = trees[1]
+
+    local root = tree:root()
+    local ctx = { bufnr = bufnr, mappings = mappings }
+    for node in root:iter_children() do
+        M.parse_node(node, ctx)
+    end
+
+    return mappings
 end
 
 return M
--- tests/plenary/makemapper_spec.lua
+++ tests/plenary/makemapper_spec.lua
@@ -1,5 +1,14 @@
-describe("makemapper", function()
-    it("rocks", function()
-        assert.is_true(true)
+local makefile = require("makemapper.makefile")
+
+local test_buffer = function(lines)
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    return buf
+end
+
+describe("parse_comment", function()
+    it("basically works", function()
+        local result = makefile.parse_comment("# my comment nvim_map(ab)")
+        assert.equal(result, "ab")
     end)
 end)
