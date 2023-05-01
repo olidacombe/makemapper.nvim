b6372b3e6f14ee6798fb337f99a129d4bb39f4c6
Oli Dacombe <olidacombe@gmail.com>
2023-05-01 23:40:51 +0100

    cleanups

--- lua/makemapper/init.lua
+++ lua/makemapper/init.lua
@@ -20,10 +20,33 @@ local default_opts = {
 
 local opts
 
+-- store parsed mappings so that we can remove old ones on refresh events
+M._mappings = {}
+
 local set_mappings = function(m)
-    local mappings = m or {}
-    for target, suffix in pairs(mappings) do
+    -- first clean up any existing
+    for _, suffix in pairs(M._mappings) do
+        vim.keymap.del("n", opts.prefix .. suffix)
+    end
+    local has_which_key, which_key = pcall(require, "which-key")
+    if has_which_key then
+        local slated_mappings = {}
+        for _, suffix in pairs(M._mappings) do
+            slated_mappings[opts.prefix .. suffix] = "which_key_ignore"
+        end
+        which_key.register(slated_mappings)
+    end
+    -- then set the new ones
+    M._mappings = m or {}
+    for target, suffix in pairs(M._mappings) do
         vim.keymap.set("n", opts.prefix .. suffix, M.make_runner(target), { desc = target })
+    end
+    if has_which_key then
+        local mappings = {}
+        for target, suffix in pairs(M._mappings) do
+            mappings[opts.prefix .. suffix] = target
+        end
+        which_key.register(mappings)
     end
 end
 
